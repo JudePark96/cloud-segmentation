d0b034a7921259c737e8d7b51f07e78a16979725
Jude Park <park.jude.96@gmail.com>
2019-11-12 15:24:24 +0900

    #2 closes, dilation, padding 12 인 convolution layer 추가

--- blocks/conv.py
+++ blocks/conv.py
@@ -28,12 +28,15 @@ class AtrousConvolutionBlock(nn.Module):
         self.conv_3x3_3 = nn.Conv2d(self.in_channel, self.out_channel, kernel_size=3, stride=1, padding=9, dilation=9)
         self.bn_conv_3x3_3 = nn.BatchNorm2d(self.out_channel)
 
-        self.avg_pool = nn.AdaptiveMaxPool2d(1)
+        self.conv_3x3_4 = nn.Conv2d(self.in_channel, self.out_channel, kernel_size=3, stride=1, padding=12, dilation=12)
+        self.bn_conv_3x3_4 = nn.BatchNorm2d(self.out_channel)
+
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
 
         self.conv_1x1_2 = nn.Conv2d(self.in_channel, self.out_channel, kernel_size=1)
         self.bn_conv_1x1_2 = nn.BatchNorm2d(self.out_channel)
 
-        self.conv_1x1_3 = nn.Conv2d(5 * self.out_channel, self.out_channel, kernel_size=1)
+        self.conv_1x1_3 = nn.Conv2d(6 * self.out_channel, self.out_channel, kernel_size=1)
         self.bn_conv_1x1_3 = nn.BatchNorm2d(self.out_channel)
 
         self.conv_1x1_4 = nn.Conv2d(self.out_channel, num_classes, kernel_size=1)
@@ -46,14 +49,15 @@ class AtrousConvolutionBlock(nn.Module):
         out_3x3_1 = F.relu(self.bn_conv_3x3_1(self.conv_3x3_1(feature_map)))
         out_3x3_2 = F.relu(self.bn_conv_3x3_2(self.conv_3x3_2(feature_map)))
         out_3x3_3 = F.relu(self.bn_conv_3x3_3(self.conv_3x3_3(feature_map)))
+        out_3x3_4 = F.relu(self.bn_conv_3x3_4(self.conv_3x3_4(feature_map)))
 
-        out_img = self.avg_pool(feature_map)
+        out_img = self.max_pool(feature_map)
         out_img = F.relu(self.bn_conv_1x1_2(self.conv_1x1_2(out_img)))
         out_img = F.interpolate(out_img, size=(feature_map_h, feature_map_w), align_corners=True, mode='bilinear')
 
-        print([i.shape for i in[out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_img]])
+        print(f'{[out.shape for out in [out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_3x3_4, out_img]]}')
 
-        out = torch.cat([out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_img], 1)
+        out = torch.cat([out_1x1, out_3x3_1, out_3x3_2, out_3x3_3, out_3x3_4, out_img], 1)
         out = F.relu(self.bn_conv_1x1_3(self.conv_1x1_3(out)))
         out = self.conv_1x1_4(out)
 
